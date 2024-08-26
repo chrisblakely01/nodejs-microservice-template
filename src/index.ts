36968df90a75a5df9d5387cf75b8e7c0605396e7
Chris Blakely <chrisblakely01@users.noreply.github.com>
2024-08-26 11:18:36 +0100

    adding cookie-parser

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import healthRoutes from "./routes/healthRoutes";
 
 dotenv.config();
@@ -8,6 +9,7 @@ dotenv.config();
 const app = express();
 const port = 3000;
 
+app.use(cookieParser());
 app.use(express.json());
 app.use(cors());
 app.use("/health", healthRoutes);
